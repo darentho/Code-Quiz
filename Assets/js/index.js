c26dc8b0a01e9dde87476e8f8809c7bc26fcba63
darentho <106845099+darentho@users.noreply.github.com>
2022-07-14 23:23:48 -0500

    added the questions array

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -47,3 +47,31 @@ const ans4Btn = document.querySelector("#answer-4");
 
 
 
+// array for the five questions. 
+const questions = [
+    {
+        question: "Primitive data types do not include?:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        question: "What do the if/else statments need to work? _______.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        question: "In Javascript, Arrays store: ______.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
+    }
+];
